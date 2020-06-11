a89bc3dc19a7bf3936fe9ae302e18e18a31a5596
moore <contact0moore.com>
2020-06-11 12:20:21 -0600

    Practica final 11/06/2020

---
+++ holamundo/holamundo/src/main/java/com/Antony/holamundo/Model/Persona.java
@@ -0,0 +1,36 @@
+package com.Antony.holamundo.Model;
+
+public class Persona {
+private String Nombre;
+private String Apellido;
+private String telefono;
+private String email;
+public String getNombre() {
+	return Nombre;
+}
+public void setNombre(String nombre) {
+	Nombre = nombre;
+}
+public String getApellido() {
+	return Apellido;
+}
+public void setApellido(String apellido) {
+	Apellido = apellido;
+}
+public String getTelefono() {
+	return telefono;
+}
+public void setTelefono(String telefono) {
+	this.telefono = telefono;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+
+
+	
+	
+}
--- holamundo/holamundo/src/main/java/com/Antony/holamundo/holamundo/Controlador.java
+++ holamundo/holamundo/src/main/java/com/Antony/holamundo/holamundo/Controlador.java
@@ -1,8 +1,13 @@
 package com.Antony.holamundo.holamundo;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.Antony.holamundo.Model.Persona;
 
 
 @Controller
@@ -10,9 +15,25 @@ public class Controlador {
 
 	@GetMapping("/")
 	public String Inicio(Model model) {
-		String mensaje = "Hola desde el controlador";
-		model.addAttribute("saludo",mensaje);
+		Persona person = new Persona();
+		person.setNombre("Antony");
+		person.setApellido("Fuentes");
+		person.setTelefono("7812-9878");
+		person.setEmail("Antony@gmail.com");
 		
+		Persona person2 = new Persona();
+		person2.setNombre("Enmanuel");
+		person2.setApellido("Villacorta");
+		person2.setTelefono("9899-7678");
+		person2.setEmail("Antony@gmail.com");
+		
+		List personas = Arrays.asList(person,person2);
+		
+		
+		String mensaje = "Hola desde el controlador Antony";
+		model.addAttribute("saludo",mensaje);
+		//model.addAttribute("persona",person); 
+		model.addAttribute("personas",personas); 
 		
 		return "index";
 	}
